1647c20ea2adab25a2bba2d714656b63095593f5
sounishnath003 <flock.sinasini@gmail.com>
2021-04-19 00:23:53 +0530

    Deleted: Unused Folders for now

--- backend-dashboard/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'c22f47841869e6b5769b495e9ea013ad'),
-    },
-  },
-});
--- backend-dashboard/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || 'a276ab89-bd03-48f2-a6f1-20580d5669e0'
-};
