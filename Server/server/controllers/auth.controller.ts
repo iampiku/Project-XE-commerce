04da6fd1b111f9efa9b54b0153db20f61bc42c31
sounishnath003 <flock.sinasini@gmail.com>
2021-05-05 13:24:09 +0530

    FEAT: Upgrade Profile to User -> Seller can now be done!

--- Server/server/controllers/auth.controller.ts
+++ Server/server/controllers/auth.controller.ts
@@ -1,176 +1,193 @@
-import { Router } from "express";
-import { Address, Order, OrderItem, User } from "../../database/schema";
-import { Role, RoleInterface } from "../../database/schema/role.schema";
+import {Router} from "express";
+import {Address , Order , OrderItem , User} from "../../database/schema";
+import {Role , RoleInterface} from "../../database/schema/role.schema";
 import {
-  compareWithHashifiedPassword,
-  FORBIDDEN,
-  generateAuthToken,
-  INTERNAL_SERVER_ERROR,
-  OK,
-  requiresAuth,
-  setAuthorizationHeader,
-  SignupInterface,
-  SUCCESS,
-  warn,
+    compareWithHashifiedPassword ,
+    FORBIDDEN ,
+    generateAuthToken ,
+    INTERNAL_SERVER_ERROR ,
+    Next ,
+    OK ,
+    RequestInterface ,
+    requiresAuth ,
+    requiresToBeAdmin ,
+    ResponseInterface ,
+    setAuthorizationHeader ,
+    SignupInterface ,
+    SUCCESS ,
+    warn ,
 } from "../utils";
 
-const router = Router();
+const router = Router ();
 
 // [POST] Create User Account
-router.post("/signup", async (req, res, next) => {
-  try {
-    const { name, email, password, username } = req.body as SignupInterface;
+router.post ("/signup" , async (req , res , next) => {
+    try {
+        const {name , email , password , username} = req.body as SignupInterface;
 
-    // Setting up Default Role as ~ CustomerRole
-    const customerRole = await Role.findOne({
-      where: { role: RoleInterface.CUSTOMER },
-    });
+        // Setting up Default Role as ~ CustomerRole
+        const customerRole = await Role.findOne ({
+            where :{role :RoleInterface.CUSTOMER} ,
+        });
 
-    const payload = {
-      name,
-      email,
-      roleId: (customerRole as any).dataValues.id,
-      username,
-      password,
-    };
+        const payload = {
+            name ,
+            email ,
+            roleId :(customerRole as any).dataValues.id ,
+            username ,
+            password ,
+        };
 
-    // Some perfoming Sanity Checks
-    // If Username Exists
-    // Or UserEmail Exists
-    const checkIfUserNameExists = await User.findOne({
-      where: { username },
-    });
-    if (checkIfUserNameExists) {
-      return warn(res, INTERNAL_SERVER_ERROR, "Username already taken");
+        // Some perfoming Sanity Checks
+        // If Username Exists
+        // Or UserEmail Exists
+        const checkIfUserNameExists = await User.findOne ({
+            where :{username} ,
+        });
+        if (checkIfUserNameExists) {
+            return warn (res , INTERNAL_SERVER_ERROR , "Username already taken");
+        }
+        const checkIfEmailExists = await User.findOne ({
+            where :{email} ,
+        });
+        if (checkIfUserNameExists || checkIfEmailExists) {
+            return warn (res , INTERNAL_SERVER_ERROR , "User email is already taken");
+        }
+
+        const resp: any = await User.create (payload);
+        const token = generateAuthToken (resp);
+
+        setAuthorizationHeader (res , token);
+
+        return res.status (OK).send ({
+            token ,
+            ...SUCCESS ,
+            userId :resp.id ,
+            isLoggedIn :true ,
+            message :`user account has been created!` ,
+        });
+    } catch (error) {
+        warn (res , FORBIDDEN , "User account already exists!");
     }
-    const checkIfEmailExists = await User.findOne({
-      where: { email },
-    });
-    if (checkIfUserNameExists || checkIfEmailExists) {
-      return warn(res, INTERNAL_SERVER_ERROR, "User email is already taken");
-    }
-
-    const resp: any = await User.create(payload);
-    const token = generateAuthToken(resp);
-
-    setAuthorizationHeader(res, token);
-
-    return res.status(OK).send({
-      token,
-      ...SUCCESS,
-      userId: resp.id,
-      isLoggedIn: true,
-      message: `user account has been created!`,
-    });
-  } catch (error) {
-    warn(res, FORBIDDEN, "User account already exists!");
-  }
-  next();
+    next ();
 });
 
 // [POST] User Login
-router.post("/login", async (req, res, next) => {
-  try {
-    const { username, password } = req.body;
-    if (username === "" || password === "") {
-      warn(res, FORBIDDEN, `fields are not formatted correctly`);
+router.post ("/login" , async (req , res , next) => {
+    try {
+        const {username , password} = req.body;
+        if (username === "" || password === "") {
+            warn (res , FORBIDDEN , `fields are not formatted correctly`);
+        }
+
+        // check in database if DataFragment Found
+        const checkIfUserNameExists: any = await User.findOne ({
+            where :{username} ,
+        });
+
+        if (checkIfUserNameExists === null) {
+            warn (res , FORBIDDEN , "username does not exists");
+        }
+
+        const passwordMatch = await compareWithHashifiedPassword (
+            password ,
+            checkIfUserNameExists
+        );
+
+        if (passwordMatch) {
+            const payload = {...checkIfUserNameExists};
+            const token: string = generateAuthToken (payload);
+            setAuthorizationHeader (res , token);
+            return res.status (OK).send ({
+                token ,
+                ...SUCCESS ,
+                isLoggedIn :true ,
+                userId :checkIfUserNameExists.id ,
+            });
+        } else {
+            warn (res , FORBIDDEN , "password does not matching!");
+        }
+    } catch (error) {
+        warn (res , FORBIDDEN , "Credentials are not matching!");
     }
-
-    // check in database if DataFragment Found
-    const checkIfUserNameExists: any = await User.findOne({
-      where: { username },
-    });
-
-    if (checkIfUserNameExists === null) {
-      warn(res, FORBIDDEN, "username does not exists");
-    }
-
-    const passwordMatch = await compareWithHashifiedPassword(
-      password,
-      checkIfUserNameExists
-    );
-
-    if (passwordMatch) {
-      const payload = { ...checkIfUserNameExists };
-      const token: string = generateAuthToken(payload);
-      setAuthorizationHeader(res, token);
-      return res.status(OK).send({
-        token,
-        ...SUCCESS,
-        isLoggedIn: true,
-        userId: checkIfUserNameExists.id,
-      });
-    } else {
-      warn(res, FORBIDDEN, "password does not matching!");
-    }
-  } catch (error) {
-    warn(res, FORBIDDEN, "Credentials are not matching!");
-  }
-  next();
+    next ();
 });
 
 // [POST] Reset Password Link
-router.post("/reset-password", async (req, res, next) => {
-  try {
-    const { username, email } = req.body;
-    const resp = await User.findOne({ where: { username, email } });
+router.post ("/reset-password" , async (req , res , next) => {
+    try {
+        const {username , email} = req.body;
+        const resp = await User.findOne ({where :{username , email}});
 
-    const resetToken = generateAuthToken(resp);
-    return res.status(OK).send({
-      resetToken,
-      ip: req.ip,
-      timestamp: Date.now(),
-      ...SUCCESS,
-    });
-  } catch (error) {
-    warn(res, FORBIDDEN, "Credentials are not matching!");
-  }
+        const resetToken = generateAuthToken (resp);
+        return res.status (OK).send ({
+            resetToken ,
+            ip :req.ip ,
+            timestamp :Date.now () ,
+            ...SUCCESS ,
+        });
+    } catch (error) {
+        warn (res , FORBIDDEN , "Credentials are not matching!");
+    }
 
-  next();
+    next ();
 });
 
 // [POST] When User wants LoggedOut
-router.post("/logout", requiresAuth, async (req, res, next) => {
-  try {
-    return res.status(OK).send({ isLoggedIn: false, ...SUCCESS });
-  } catch (error) {
-    warn(res, FORBIDDEN, "You are not Authorized");
-  }
-  next();
+router.post ("/logout" , requiresAuth , async (req , res , next) => {
+    try {
+        return res.status (OK).send ({isLoggedIn :false , ...SUCCESS});
+    } catch (error) {
+        warn (res , FORBIDDEN , "You are not Authorized");
+    }
+    next ();
 });
 
 // [POST] Getting all orders for the user
-router.get("/user/orders/all", requiresAuth, async (req, res, next) => {
-  try {
-    const { userId } = req as any;
+router.get ("/user/orders/all" , requiresAuth , async (req , res , next) => {
+    try {
+        const {userId} = req as any;
 
-    /** Getting all the orders from OrderSchema for UserId = `id` */
-    const allOrders = await Order.findAll({
-      include: [{ model: OrderItem, as: "orderItems" }, { model: Address }],
-      where: { userId },
-      order: [["createdAt", "DESC"]],
-    });
-    return res.status(OK).send({ ...SUCCESS, allOrders, userId });
-  } catch (error) {
-    warn(res, FORBIDDEN, error || "You are not authorized");
-  }
-  next();
+        /** Getting all the orders from OrderSchema for UserId = `id` */
+        const allOrders = await Order.findAll ({
+            include :[{model :OrderItem , as :"orderItems"} , {model :Address}] ,
+            where :{userId} ,
+            order :[["createdAt" , "DESC"]] ,
+        });
+        return res.status (OK).send ({...SUCCESS , allOrders , userId});
+    } catch (error) {
+        warn (res , FORBIDDEN , error || "You are not authorized");
+    }
+    next ();
 });
 
 // [Post] To Create several address for user which userId (requiresAuth.middleware)
-router.post("/user/address/create", requiresAuth, async (req, res, next) => {
-  try {
-    const resp = await Address.create(req.body);
-    return res
-      .status(OK)
-      .send({ ...SUCCESS, resp, message: `address has been created!` });
-  } catch (error) {
-    warn(res, FORBIDDEN, error || "You are not authorized");
-  }
-  next();
+router.post ("/user/address/create" , requiresAuth , async (req , res , next) => {
+    try {
+        const resp = await Address.create (req.body);
+        return res
+            .status (OK)
+            .send ({...SUCCESS , resp , message :`address has been created!`});
+    } catch (error) {
+        warn (res , FORBIDDEN , error || "You are not authorized");
+    }
+    next ();
 });
 
 // [POST upgrading an existing user to a SELLER!
-
+router.post ('/upgrade-profile' , requiresAuth , requiresToBeAdmin , async (req: RequestInterface , res: ResponseInterface , next: Next) => {
+    try {
+        const {userId} = req.body;
+        const sellerRoleInstance = await Role.findOne ({where :{role :RoleInterface.SELLER}});
+        const sellerRoleId = sellerRoleInstance?.getDataValue ('id');
+        const userInstance = await User.findOne ({where :{id : userId}});
+        (userInstance as any).roleId = sellerRoleId;
+        await userInstance?.save ();
+        return res.status (OK).send ({...SUCCESS , message :`user has been upgraded to a seller!`});
+    } catch (e) {
+        return warn (res , FORBIDDEN , e || 'You are not authorized as Admin Profile');
+    }
+    next ();
+})
 
 export default router;
--- Server/server/server.ts
+++ Server/server/server.ts
@@ -1,24 +1,24 @@
 import cors from "cors";
-import express, {json, urlencoded} from "express";
+import express , {json , urlencoded} from "express";
 import fileUpload from "express-fileupload";
 import morgan from "morgan";
 import {cpus} from "os";
 import {pid} from "process";
 import {connectToDatabase} from "../database";
-import {buildAssociationsBetweenSchemas, User} from "../database/schema";
-import {Role, RoleInterface} from "../database/schema/role.schema";
+import {buildAssociationsBetweenSchemas , User} from "../database/schema";
+import {Role , RoleInterface} from "../database/schema/role.schema";
 import {APIController} from "./controllers";
 
 export class Server {
     private readonly PORT = 5000 || process.env.PORT;
-    private readonly workers = cpus().length;
-    private app = express();
+    private readonly workers = cpus ().length;
+    private app = express ();
 
     constructor() {
         /**
          * INFO:
          * Can be enable in Production
-         * But for developement
+         * But for development
          * I think it is not required for now!!
          */
         // if (isMaster) {
@@ -28,29 +28,30 @@ export class Server {
         // } else {
         //   this.internalServerStart();
         // }
-        this.internalServerStart();
+        this.internalServerStart ();
     }
 
-    async serverConfig() {
-        // express-fileupload setup middleware
-        this.app.use(
-            fileUpload({
-                useTempFiles: true,
-                tempFileDir: `/tmp/`,
-            })
+    private async serverConfig() {
+        debugger
+        // express-file-upload setup middleware
+        this.app.use (
+            fileUpload ( {
+                useTempFiles : true ,
+                tempFileDir : `/tmp/` ,
+            } )
         );
-        this.app.use(json());
-        this.app.use(cors());
-        this.app.use(morgan("dev"));
-        this.app.use(urlencoded({extended: false}));
+        this.app.use ( json () );
+        this.app.use ( cors () );
+        this.app.use ( morgan ( "dev" ) );
+        this.app.use ( urlencoded ( { extended : false } ) );
     }
 
     private async internalServerStart() {
         try {
-            await this.serverConfig();
-            await connectToDatabase();
-            await (async () => {
-                buildAssociationsBetweenSchemas();
+            await this.serverConfig ();
+            await connectToDatabase ();
+            await ( async () => {
+                await buildAssociationsBetweenSchemas ();
                 /** Building up the UserPermissionRoles
                  * Pure RoleBased Authentication
                  * When ever user tries to signup - he/she will
@@ -60,49 +61,57 @@ export class Server {
                  *
                  * others who else wanna sell, can be associated with`RoleInterface.SELLER` permission
                  */
-                await Role.findOne({where: {role: RoleInterface.CUSTOMER}}).then((m) => {
+                await Role.findOne ({where :{role :RoleInterface.CUSTOMER}}).then ((m) => {
                     if (m == null) {
                         (async function () {
                             // await Role.destroy({ truncate: true });
-                            await Role.create({role: "ADMIN"});
-                            await Role.create({role: "CUSTOMER"});
-                            await Role.create({role: "SELLER"});
-                        })();
+                            await Role.create ({role :"ADMIN"});
+                            await Role.create ({role :"CUSTOMER"});
+                            await Role.create ({role :"SELLER"});
+                        }) ();
                     }
                 });
-            })();
+            }) ();
 
-            /** Creating Admin User OneTime */
-            await Role.findOne({where: {role: RoleInterface.ADMIN}}).then(async (m) => {
-                if (m !== null) {
-                  console.log({roleAdminId: (m as any).dataValues.id})
-                    const isAdminPresent = await User.findOne({
-                        where: {
-                            username: 'ironman@work',
-                            email: 'admin@ecommercex.com',
-                        }
-                    });
-                  if (isAdminPresent === null)
-                    await User.create({
-                      username: 'ironman@work',
-                      password: '1234',
-                      name: 'ADMIN',
-                      email: 'admin@ecommercex.com',
-                      roleId: (m as any).dataValues.id
-                    });
-                }
-            })
-          console.log(`\n### Admin User is settled up with email: ironman@work`);
+            await this.createAdminUserIfNotExists ();
 
-            this.app.use("/api", APIController);
+            this.app.use ( "/api" , APIController );
 
-            this.app.listen(this.PORT, () =>
-                console.log(
+            this.app.listen ( this.PORT , () =>
+                console.log (
                     `[ PID:${pid} ] 🚀 Server already started on http://localhost:${this.PORT}`
                 )
             );
         } catch (error) {
-            console.error({error});
+            console.error ( { error } );
+        }
+    }
+
+    private async createAdminUserIfNotExists() {
+        /** Creating Admin User OneTime */
+        try {
+            await Role.findOne ( { where : { role : RoleInterface.ADMIN } } ).then ( async ( m ) => {
+                if ( m !== null ) {
+                    console.log ( { roleAdminId : ( m as any ).dataValues.id } )
+                    const isAdminPresent = await User.findOne ( {
+                        where : {
+                            username : 'ironman@work' ,
+                            email : 'admin@ecommercex.com' ,
+                        }
+                    } );
+                    if ( isAdminPresent === null )
+                        await User.create ( {
+                            username : 'ironman@work' ,
+                            password : '1234' ,
+                            name : 'ADMIN' ,
+                            email : 'admin@ecommercex.com' ,
+                            roleId : ( m as any ).dataValues.id
+                        } );
+                }
+            } )
+            console.log ( `\n### Admin User is settled up with email: ironman@work` );
+        } catch (e) {
+            console.error ( e )
         }
     }
 }
